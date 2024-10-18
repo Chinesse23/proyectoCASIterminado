dcb8690416fd7f61720c2c7f29d58e60bfff510e
Chinesse23 <czuazua18@gmail.com>
2024-10-18 07:51:07 -1100

    nuevos cambios en los archivos

--- views/js/login.js
+++ views/js/login.js
@@ -17,22 +17,31 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
       const data = await response.json();
       if (response.ok) {
           const successBox = document.getElementById('loginSuccessBox');
-          successBox.textContent = 'Login successful';
-          successBox.style.display = 'block';
-
+          if (successBox) {
+              successBox.textContent = 'Login successful';
+              successBox.style.display = 'block';
+          }
           setTimeout(() => {
-              window.location.href = data.role === 'admin' ? '/admin' : '/';
+              if (data.role === 'admin') {
+                  window.location.href = '/admin';
+              } else {
+                  window.location.href = '/';
+              }
           }, 2000); // Redirige después de 2 segundos
       } else {
           const alertBox = document.getElementById('loginAlertBox');
-          alertBox.textContent = data.message || 'Ocurrió un error desconocido.';
-          alertBox.style.display = 'block';
+          if (alertBox) {
+              alertBox.textContent = data.message || 'Ocurrió un error desconocido.';
+              alertBox.style.display = 'block';
+          }
       }
   } catch (error) {
       console.error('Error:', error);
       const alertBox = document.getElementById('loginAlertBox');
-      alertBox.textContent = 'Ocurrió un error. Por favor, intenta nuevamente.';
-      alertBox.style.display = 'block';
+      if (alertBox) {
+          alertBox.textContent = 'Ocurrió un error. Por favor, intenta nuevamente.';
+          alertBox.style.display = 'block';
+      }
   }
 });
 
@@ -44,7 +53,7 @@ ScrollReveal().reveal('.container.glass', {
   delay: 200,
   easing: 'ease-in-out'
 });
-ScrollReveal().reveal('.top-header', {
+ScrollReveal().reveal('.top-header', { 
   origin: 'top',
   distance: '30px',
   duration: 800,
